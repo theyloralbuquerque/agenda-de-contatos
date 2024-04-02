b902eb8a5df6bf718afff20d7ef3812cb2d2c850
Theylor Albuquerque <theylor18.albuquerque@gmail.com>
2024-04-02 10:45:56 -0300

    feat: implementation of reanding and deleting contacts

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -33,7 +33,7 @@ exports.editIndex = async function(req, res) {
     if(!contato) return res.render('404');
 
     res.render('contato', { contato });
-}
+};
 
 exports.edit = async function(req, res) {
     try {
@@ -54,4 +54,15 @@ exports.edit = async function(req, res) {
         console.log(e);
         res.render('404');
     }
-} 
+};
+
+exports.delete = async function(req, res) {
+    if(!req.params.id) return res.render('404');
+
+    const contato = await Contato.delete(req.params.id);
+    if(!contato) return res.render('404');
+
+    req.flash('success', 'Contato apagado com sucesso.');
+    req.session.save(() => res.redirect(`/`));
+    return;
+};
